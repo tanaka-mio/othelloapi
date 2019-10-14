a7bdd2a1d2566eca0df77809688659916bb2473e
tanakamio <tanakamio@192.168.3.9>
2019-10-14 18:48:41 +0900

    20191014_homework1

--- src/main/java/com/example/demo/app/controller/RestApiController.java
+++ src/main/java/com/example/demo/app/controller/RestApiController.java
@@ -3,20 +3,50 @@ package com.example.demo.app.controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.app.resource.OthelloStone;
 
+
 @RestController
 @RequestMapping("api/sample")
 public class RestApiController {
+	
+	public int [][] OthelloStone = {
+	    	{0, 0, 0, 0, 0, 0, 0, 0},
+	        {0, 0, 0, 0, 0, 0, 0, 0},
+	        {0, 0, 0, 0, 0, 0, 0, 0},
+	        {0, 0, 0, 1, -1, 0, 0, 0},
+	        {0, 0, 0, -1, 1, 0, 0, 0},
+	        {0, 0, 0, 0, 0, 0, 0, 0},
+	        {0, 0, 0, 0, 0, 0, 0, 0},
+	        {0, 0, 0, 0, 0, 0, 0, 0}
+    	};
 
+	@RequestMapping(value = "/getMessage", method = RequestMethod.GET)
+    @ResponseBody
+    @CrossOrigin
+    public OthelloStone getMessage() {
+    	OthelloStone othelloStone = new OthelloStone("1", "黒");
+        return othelloStone;
+    }
+	
     @RequestMapping(value = "/getOthelloStone", method = RequestMethod.GET)
     @ResponseBody
     @CrossOrigin
-    public OthelloStone getOthelloStone() {
-    	OthelloStone othelloStone = new OthelloStone("1", "黒");
-        return othelloStone;
+    public int[][] getOthelloStone() {
+        return OthelloStone;
+    }
+    
+    @RequestMapping(value = "/hitOthelloStone", method = RequestMethod.GET)
+    @ResponseBody
+    @CrossOrigin
+    public String hitOthelloStone(
+    		@RequestParam("hitX") int hitX,
+    		@RequestParam("hitY") int hitY) {
+    	OthelloStone[hitY][hitX] = -1;
+        return "あなたの番です";
     }
 }
